c8c89d4411d8b02f725eca0d7df0b9c83dcc9c03
Haven Gibbs <oilpastel701@gmail.com>
2023-04-10 21:41:41 -0500

    data button animations

---
+++ Assets/Scripts/UI/AnimationScripts/OpenDataTween.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDataTween : MonoBehaviour
+{
+    [SerializeField] public GameObject parentToggle;
+
+    [SerializeField] public GameObject content;
+
+    [SerializeField] public float openWidth;
+    [SerializeField] public float openHeight;
+
+    [SerializeField] public float yDist;
+    [SerializeField] public float xDist;
+    [SerializeField] public float duration;
+    [SerializeField] public float closeDuration;
+
+    protected bool tog = false;
+
+    public void SwitchTog()
+    {
+        tog = !tog;
+    }
+
+    public void Move()
+    {
+        SwitchTog();
+
+        if (!tog)
+            Close();
+        else if (tog)
+            Open();
+    }
+
+    public void Open()
+    {
+        this.gameObject.SetActive(true);
+        //LeanTween.moveX(this.gameObject, xDist, duration / 3);
+        LeanTween.size(this.GetComponent<RectTransform>(), new Vector2(openWidth, 10), duration / 4).setEaseInOutQuint();
+        LeanTween.size(this.GetComponent<RectTransform>(), new Vector2(openWidth, openHeight), duration / 3).setDelay(duration / 4).setEaseInOutCubic();
+        if(content != null)
+            LeanTween.scaleY(content, 1f, duration / 3).setDelay(duration / 3).setEaseInOutCubic();
+    }
+
+    public void Close()
+    {
+        LeanTween.size(this.GetComponent<RectTransform>(), new Vector2(openWidth, 10), closeDuration / 4).setEaseInOutCubic();
+        LeanTween.size(this.GetComponent<RectTransform>(), new Vector2(0, 10), closeDuration / 3).setDelay(closeDuration / 4).setEaseInOutQuint();
+
+        if (content != null)
+            LeanTween.scaleY(content, 0f, duration / 3).setEaseInOutCubic();
+        StartCoroutine(WaitForAnim(duration));
+    }
+
+    public IEnumerator WaitForAnim(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        this.gameObject.SetActive(false);
+        //this.gameObject.transform.position = new Vector2(155,32);
+        //LeanTween.size(this.GetComponent<RectTransform>(), new Vector2(210, 150), 0);
+    }
+
+}
