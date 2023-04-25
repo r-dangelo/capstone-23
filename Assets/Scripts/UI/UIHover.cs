af45035c98aefb003cf733b59b5c3ece0d14eec9
Haven Gibbs <oilpastel701@gmail.com>
2023-04-25 14:34:16 -0500

    update assets

--- Assets/Scripts/UI/UIHover.cs
+++ Assets/Scripts/UI/UIHover.cs
@@ -6,14 +6,17 @@ using UnityEngine.EventSystems;
 public class UIHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private GameObject hoverObject;
+    [SerializeField] private GameObject hoverText;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         hoverObject.SetActive(true);
+        hoverText.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         hoverObject.SetActive(false);
+        hoverText.SetActive(false);
     }
 }
