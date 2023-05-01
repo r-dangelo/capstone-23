f94baac8b53a9559f7d5f23d9d70a3cd040bc55f
Haven Gibbs <oilpastel701@gmail.com>
2023-04-30 21:45:49 -0500

    bro I've almost got it

--- Assets/Scripts/UI/ClassifyNav.cs
+++ Assets/Scripts/UI/ClassifyNav.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class ClassifyNav : MonoBehaviour
 {
     [SerializeField] public ClassifyBackBtn backBtnScript;
+    [SerializeField] public GameObject parentTog;
     [SerializeField] public GameObject nextTog;
     [SerializeField] public GameObject nextPanel;
     [SerializeField] public GameObject nextTaxa;
@@ -14,7 +15,8 @@ public class ClassifyNav : MonoBehaviour
 
     public void ProgressClassification()
     {
-        //backBtnScript.SetAsPrevToggle(this.gameObject);
+        if(parentTog != null)
+            backBtnScript.SetAsPrevToggle(parentTog.gameObject);
 
         if (otherTaxa != null)
         {
@@ -25,13 +27,27 @@ public class ClassifyNav : MonoBehaviour
         if(nextTaxa != null)
             nextTaxa.SetActive(true);
         if (nextPanel != null)
-            nextPanel.GetComponent<OpenDataTween>().Move();
+            nextPanel.GetComponent<OpenDataTween>().Open();
         if (nextTaxa != null)
             for (int i=0;i<nextTaxa.transform.childCount;i++)
-                nextTaxa.transform.GetChild(i).GetComponent<OpenDataTween>().Move();
+                if(nextTaxa.transform.GetChild(i).gameObject.activeSelf)
+                    nextTaxa.transform.GetChild(i).GetComponent<OpenDataTween>().Open();
 
         if (nextTog != null)
-            backBtnScript.MoveBackBtn(nextTog);
+          backBtnScript.MoveBackBtn(nextTog);
+
+    }
+
+    public void CloseNextBranch()
+    {
+        if (nextPanel != null)
+            nextPanel.GetComponent<OpenDataTween>().Close();
+        if (otherTaxa != null)
+            for (int i = 0; i < otherTaxa.Count; i++)
+                if (otherTaxa[i].gameObject.activeSelf)
+                    for (int x = 0; x<otherTaxa[i].transform.childCount; x++)
+                        if(otherTaxa[i].transform.GetChild(x).gameObject.activeSelf)
+                            otherTaxa[i].transform.GetChild(x).GetComponent<OpenDataTween>().Close();
 
     }
 
