6a4ef05b5eb071399cd4a28cc2d3a0f905c0390e
l-dangelo <lkd180001@utdallas.edu>
2023-05-01 18:39:05 -0500

    Add Journal Tracker Functionality
    Still pretty basic for now, until I have a more clear idea of what we want

--- Assets/Scripts/JournalTracker.cs
+++ Assets/Scripts/JournalTracker.cs
@@ -4,5 +4,81 @@ using UnityEngine;
 
 public class JournalTracker : MonoBehaviour
 {
+    [HideInInspector]
+    public float anamalia = 0f;
+    [HideInInspector]
+    public float fungi = 0f;
 
+    [HideInInspector]
+    public float chordata = 0f;
+    [HideInInspector]
+    public float arthropoda = 0f;
+
+    [HideInInspector]
+    public float reptilia = 0f;
+    [HideInInspector]
+    public float mammalia = 0f;
+    [HideInInspector]
+    public float arachnida = 0f;
+    [HideInInspector]
+    public float malacostraca = 0f;
+
+    [HideInInspector]
+    public float decapoda = 0f;
+    [HideInInspector]
+    public float carnivora = 0f;
+
+    public void addKingdom(float amount, int kingdomSeeEnum)
+    {
+        switch (kingdomSeeEnum) {
+            case 0:
+                anamalia += amount;
+                break;
+            case 1:
+                fungi += amount;
+                break;
+        }
+    }
+
+    public void addPhylum(float amount, int phylumSeeEnum)
+    {
+        switch(phylumSeeEnum) {
+            case 0:
+                chordata += amount;
+                break;
+            case 1:
+                arthropoda += amount;
+                break;
+        }
+    }
+
+    public void addClass(float amount, int classSeeEnum)
+    {
+        switch(classSeeEnum) {
+            case 0:
+                reptilia += amount;
+                break;
+            case 1:
+                mammalia += amount;
+                break;
+            case 2:
+                arachnida += amount;
+                break;
+            case 3:
+                malacostraca += amount;
+                break;
+        }
+    }
+
+    public void addOrder(float amount, int orderSeeEnum)
+    {
+        switch (orderSeeEnum) {
+            case 0:
+                decapoda += amount;
+                break;
+            case 1:
+                carnivora += amount;
+                break;
+        }
+    }
 }
